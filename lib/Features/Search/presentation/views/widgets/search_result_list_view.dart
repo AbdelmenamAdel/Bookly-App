f7fc1e2cfa922883be850cc1dbd641276f5d4442
AbdelmenamAdel <abdelmenamadel96@gmail.com>
2024-02-25 17:43:22 +0200

    search view done

--- lib/Features/Search/presentation/views/widgets/search_result_list_view.dart
+++ lib/Features/Search/presentation/views/widgets/search_result_list_view.dart
@@ -1,13 +1,12 @@
+import 'package:bookly/Features/Home/peresentation/views/widgets/book_list_view_item.dart';
 import 'package:flutter/material.dart';
-import 'book_list_view_item.dart';
 
-class BestSellerListView extends StatelessWidget {
-  const BestSellerListView({super.key});
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      physics: const NeverScrollableScrollPhysics(),
       padding: EdgeInsets.zero,
       itemBuilder: (context, index) => const Padding(
         padding: EdgeInsets.only(bottom: 20),
--- lib/Features/Search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/Search/presentation/views/widgets/search_view_body.dart
@@ -1,23 +1,37 @@
+import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/core/widgets/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'search_result_list_view.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30.0),
-      child: Column(
-        children: [
-          CustomTextFormField(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: CustomTextFormField(
             hintText: 'Search......',
             suffix1: const Opacity(
-                opacity: .8, child: Icon(FontAwesomeIcons.magnifyingGlass)),
+              opacity: .8,
+              child: Icon(FontAwesomeIcons.magnifyingGlass),
+            ),
           ),
-        ],
-      ),
+        ),
+        const SizedBox(height: 16),
+        const Padding(
+          padding: EdgeInsets.only(left: 30.0),
+          child: Text('Search Result', style: Styles.textStyle18),
+        ),
+        const SizedBox(height: 16),
+        const Expanded(
+          child: SearchResultListView(),
+        )
+      ],
     );
   }
 }
