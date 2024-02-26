d8543d5bf0240130f9e7a2df9b213cd86d8b7ccb
AbdelmenamAdel <abdelmenamadel96@gmail.com>
2024-02-26 17:22:20 +0200

    craete home repo methods and Failure  class

--- lib/Features/Home/data/repos/home_repository.dart
+++ lib/Features/Home/data/repos/home_repository.dart
@@ -1,4 +1,8 @@
+import 'package:bookly/Features/Home/data/models/book_model/book_model.dart';
+import 'package:bookly/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepository {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
