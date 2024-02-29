b70f88319e954d9ca8345a7d3b766fb812bafbd0
AbdelmenamAdel <abdelmenamadel96@gmail.com>
2024-02-29 20:06:37 +0200

    factor custom cached network image

--- lib/Features/Home/peresentation/views/widgets/custom_book_image.dart
+++ lib/Features/Home/peresentation/views/widgets/custom_book_image.dart
@@ -1,6 +1,5 @@
-import 'package:cached_network_image/cached_network_image.dart';
+import 'package:bookly/core/widgets/custom_cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:shimmer/shimmer.dart';
 
 class CustomBookImage extends StatelessWidget {
   const CustomBookImage({super.key, required this.imageUrl});
@@ -12,25 +11,13 @@ class CustomBookImage extends StatelessWidget {
       height: height,
       child: AspectRatio(
         aspectRatio: 2.7 / 4,
-        child:  ClipRRect(
+        child: ClipRRect(
           borderRadius: BorderRadius.circular(16),
-          child: CachedNetworkImage(
-            fit: BoxFit.fill,
+          child: CustomCachedNetworkImage(
             imageUrl: imageUrl,
-            placeholder: (context, url) => Shimmer.fromColors(
-              baseColor: Colors.grey[300]!,
-              highlightColor: Colors.grey[100]!,
-              child: Container(
-                width: 200.0,
-                height: 100.0,
-                color: Colors.white,
-              ),
-
-          ),    errorWidget: (context, url, error) => const Icon(Icons.error),
-
-    ),),
+          ),
         ),
+      ),
     );
-
   }
 }
