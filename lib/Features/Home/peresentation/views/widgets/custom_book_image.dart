87a5e88bed66bee1a777b8a0b60d29592d03ba03
AbdelmenamAdel <abdelmenamadel96@gmail.com>
2024-02-29 19:56:31 +0200

    cached network image and shimmer done

--- lib/Features/Home/peresentation/views/widgets/custom_book_image.dart
+++ lib/Features/Home/peresentation/views/widgets/custom_book_image.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class CustomBookImage extends StatelessWidget {
   const CustomBookImage({super.key, required this.imageUrl});
@@ -10,17 +12,25 @@ class CustomBookImage extends StatelessWidget {
       height: height,
       child: AspectRatio(
         aspectRatio: 2.7 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.red,
-            borderRadius: BorderRadius.circular(16),
-            image: DecorationImage(
-              fit: BoxFit.fill,
-              image: NetworkImage(imageUrl),
-            ),
-          ),
+        child:  ClipRRect(
+          borderRadius: BorderRadius.circular(16),
+          child: CachedNetworkImage(
+            fit: BoxFit.fill,
+            imageUrl: imageUrl,
+            placeholder: (context, url) => Shimmer.fromColors(
+              baseColor: Colors.grey[300]!,
+              highlightColor: Colors.grey[100]!,
+              child: Container(
+                width: 200.0,
+                height: 100.0,
+                color: Colors.white,
+              ),
+
+          ),    errorWidget: (context, url, error) => const Icon(Icons.error),
+
+    ),),
         ),
-      ),
     );
+
   }
 }
